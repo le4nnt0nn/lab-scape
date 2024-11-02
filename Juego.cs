ab7734cf7b5b1169c56f3e0a0ba011e209342f38
Antonio Cortés <71550295+le4nnt0nn@users.noreply.github.com>
2024-11-02 21:27:04 +0100

    feat : add timer

--- Juego.cs
+++ Juego.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace lab_scape
 {
@@ -12,6 +13,8 @@ namespace lab_scape
         private Jugador jugador;
         private int salidaFila;
         private int salidaColumna;
+        private int tiempoRestante = 30;
+        private Timer temporizador;
 
         // Constructor del juego
         public Juego(string usuario, int filas, int columnas)
@@ -25,16 +28,31 @@ namespace lab_scape
         // Método que inicia el bucle principal del juego
         public void Iniciar()
         {
+            // Reducir el tiempo cada segundo
+            temporizador = new Timer(1000);
+            temporizador.Elapsed += Temporizador_Elapsed; // Evento que ejecuta el método que resta tiempo (cada segundo por el Timer asociado arriba)
+            temporizador.Start();
+
             while (true)
             {
                 Console.Clear();
                 laberinto.MostrarLaberinto(jugador.Fila, jugador.Columna, salidaFila, salidaColumna, jugador.Puntuacion);
+                Console.WriteLine("Tiempo restante: " + tiempoRestante + " segundos.");
+
+                // Verificar si el tiempo se ha agotado
+                if (tiempoRestante <= 0)
+                {
+                    Console.WriteLine("¡Has perdido! Se agotó el tiempo.");
+                    temporizador.Stop();
+                    break;
+                }
 
                 // Verificar si el jugador ha llegado a la salida
                 if (jugador.Fila == salidaFila && jugador.Columna == salidaColumna)
                 {
                     Console.WriteLine("¡Felicidades, " + jugador.Usuario + "! Has escapado del laberinto.");
                     Console.WriteLine("Puntuación final: " + jugador.Puntuacion);
+                    temporizador.Stop();
                     break;
                 }
 
@@ -44,6 +62,17 @@ namespace lab_scape
 
                 // Mover al jugador
                 jugador.Mover(movimiento, laberinto);
+            }
+        }
+
+        // Método que controla el temporizador
+        private void Temporizador_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            tiempoRestante--;
+
+            if (tiempoRestante <= 0)
+            {
+                temporizador.Stop();
             }
         }
     }
